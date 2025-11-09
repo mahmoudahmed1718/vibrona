cf0b7344e4eae43d3baa28f237bd7e60f962c970
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-09 10:28:15 +0200

    create app theme file and add light theme

---
+++ lib/core/config/theme/app_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:vibrona/core/config/theme/app_colors.dart';
+
+abstract class AppTheme {
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+    ),
+  );
+}
