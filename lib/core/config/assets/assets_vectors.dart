1e20e3a737b24f95f20c9474bf440f2db3207595
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-27 12:45:27 +0200

    add file assets image and assets vector and create splash page

---
+++ lib/core/config/assets/assets_vectors.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsVectorsBottomPattern
+  /// assets/vectors/bottom_pattern.svg
+  static const String assetsVectorsBottomPattern = "assets/vectors/bottom_pattern.svg";
+
+  /// Assets for assetsVectorsHomeTopCard
+  /// assets/vectors/home_top_card.svg
+  static const String assetsVectorsHomeTopCard = "assets/vectors/home_top_card.svg";
+
+  /// Assets for assetsVectorsMoon
+  /// assets/vectors/moon.svg
+  static const String assetsVectorsMoon = "assets/vectors/moon.svg";
+
+  /// Assets for assetsVectorsSpotifyLogo
+  /// assets/vectors/spotify_logo.svg
+  static const String assetsVectorsSpotifyLogo = "assets/vectors/spotify_logo.svg";
+
+  /// Assets for assetsVectorsSun
+  /// assets/vectors/sun.svg
+  static const String assetsVectorsSun = "assets/vectors/sun.svg";
+
+  /// Assets for assetsVectorsTopPattern
+  /// assets/vectors/top_pattern.svg
+  static const String assetsVectorsTopPattern = "assets/vectors/top_pattern.svg";
+}
+
