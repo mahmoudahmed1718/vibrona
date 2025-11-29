475efd11bef0fa35793672f2bf8e0210bb15ee95
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-29 09:34:37 +0200

    add flutter_bloc and hydrated_bloc and path_provider packages and intialize them in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:vibrona/core/config/theme/app_theme.dart';
 import 'package:vibrona/presentation/splash/page/splash_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(const MyApp());
 }
 
