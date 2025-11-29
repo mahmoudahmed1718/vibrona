8855d01fdf29436bccad6a8e3c4fb639a314798b
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-29 10:30:35 +0200

    implent stack code in body of signuporsignin page

--- lib/presentation/auth/signup_or_signin.dart
+++ lib/presentation/auth/signup_or_signin.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:vibrona/core/config/assets/assets_images.dart';
+import 'package:vibrona/core/config/assets/assets_vectors.dart';
 
 class SignupOrSignin extends StatelessWidget {
   const SignupOrSignin({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: SvgPicture.asset(AssetsVectors.assetsVectorsTopPattern),
+            ),
+
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SvgPicture.asset(AssetsVectors.assetsVectorsBottomPattern),
+            ),
+            // Align(
+            //   alignment: Alignment.bottomLeft,
+            //   child: Image.asset(AssetsImages.assetsImagesIslamSobhi),
+            // ),
+          ],
+        ),
+      ),
+    );
   }
 }
