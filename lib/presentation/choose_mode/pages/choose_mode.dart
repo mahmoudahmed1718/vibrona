fc386a6fff21091f8dccc8ec0b6fa848dc0b199f
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-29 09:03:40 +0200

    feat: enhance ChooseMode page with background image, logo, and mode selection buttons

--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,9 +1,103 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vibrona/common/widgets/button/basic_app_button.dart';
+import 'package:vibrona/core/config/assets/assets_images.dart';
+import 'package:vibrona/core/config/assets/assets_vectors.dart';
 
 class ChooseMode extends StatelessWidget {
   const ChooseMode({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AssetsImages.assetsImagesBakcgorundImage),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black.withValues(alpha: 0.6),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  Image.asset(AssetsImages.assetsImagesLogo),
+                  Spacer(),
+                  Text(
+                    "Choose Mode",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 36),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ClipOval(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                          child: Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              color: Color(0xff30393c).withValues(alpha: 0.2),
+                              shape: BoxShape.circle,
+                            ),
+                            child: SvgPicture.asset(
+                              AssetsVectors.assetsVectorsMoon,
+                              fit: BoxFit.none,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(width: 50),
+                      ClipOval(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                          child: Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              color: Color(0xff30393c).withValues(alpha: 0.2),
+                              shape: BoxShape.circle,
+                            ),
+                            child: SvgPicture.asset(
+                              AssetsVectors.assetsVectorsSun,
+                              fit: BoxFit.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50),
+                  BasicAppButton(
+                    text: 'Continue',
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ChooseMode()),
+                      );
+                    },
+                  ),
+                  SizedBox(height: 40),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
