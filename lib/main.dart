8d994a7d27c7141e1e4aee097e0570f5e1ac8be3
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-29 09:52:26 +0200

    add multiblocProvider and bloc builder to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:vibrona/core/config/theme/app_theme.dart';
+import 'package:vibrona/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:vibrona/presentation/splash/page/splash_page.dart';
 
 Future<void> main() async {
@@ -19,6 +21,18 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: AppTheme.lightTheme, home: SplashPage());
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => ThemeCubit())],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, state) {
+          return MaterialApp(
+            theme: AppTheme.lightTheme,
+            themeMode: state,
+            darkTheme: AppTheme.darkTheme,
+            home: SplashPage(),
+          );
+        },
+      ),
+    );
   }
 }
--- lib/presentation/choose_mode/bloc/theme_cubit.dart
+++ lib/presentation/choose_mode/bloc/theme_cubit.dart
@@ -4,6 +4,7 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 class ThemeCubit extends HydratedCubit<ThemeMode> {
   ThemeCubit() : super(ThemeMode.system);
   void updateTheme(ThemeMode thememode) => emit(thememode);
+
   @override
   ThemeMode? fromJson(Map<String, dynamic> json) {
     // TODO: implement fromJson
