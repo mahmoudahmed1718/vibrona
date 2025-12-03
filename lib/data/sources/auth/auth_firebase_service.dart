709ac016b8abe87391bd6c5d80cc352e93954ea8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-03 16:45:56 +0200

    add firebase auth package

---
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -0,0 +1,18 @@
+abstract class AuthFirebaseService {
+  Future<void> signUp();
+  Future<void> signIn();
+}
+
+class AuthFirebaseServiceImpl implements AuthFirebaseService {
+  @override
+  Future<void> signIn() {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp() {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+}
