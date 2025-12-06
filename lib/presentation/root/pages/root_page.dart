e664588c64f4ad0b51fff520469fbefde35814b6
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-06 11:03:51 +0200

    feat: add RootPage widget as a new entry point for the application

--- lib/presentation/root/pages/root_page.dart
+++ lib/presentation/root/pages/root_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class RootPage extends StatelessWidget {
+  const RootPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold();
