1e0d8a31d842e05d4f966ae3d652bb256b433611
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-06 09:04:38 +0200

    add get_it package and create file service option

---
+++ lib/data/repostiory/auth/auth_repo_impl.dart
@@ -0,0 +1,15 @@
+import 'package:vibrona/domain/repostiory/auth/auth_repo.dart';
+
+class AuthRepoImpl extends AuthRepo {
+  @override
+  Future<void> signIn() {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp() {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+}
