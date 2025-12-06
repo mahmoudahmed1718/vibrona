0f84cfe4b5391037a1cc91964c3116b773522721
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-06 12:46:04 +0200

    add dot_env for flutter secure text

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -17,6 +18,7 @@ Future<void> main() async {
     url: 'https://xyzcompany.supabase.co',
     anonKey: 'publishable-or-anon-key',
   );
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   HydratedBloc.storage = await HydratedStorage.build(
