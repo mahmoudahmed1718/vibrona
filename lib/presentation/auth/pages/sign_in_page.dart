a2db075618387445d8db6e99140621439ae5a9ea
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-06 11:29:50 +0200

    feat: implement sign-in functionality in SignInPage and register SignInUseCase in service locator

--- lib/presentation/auth/pages/sign_in_page.dart
+++ lib/presentation/auth/pages/sign_in_page.dart
@@ -3,7 +3,11 @@ import 'package:vibrona/common/widgets/appbar/basic_appbar.dart';
 import 'package:vibrona/common/widgets/button/basic_app_button.dart';
 import 'package:vibrona/common/widgets/textFields/basic_text_field.dart';
 import 'package:vibrona/core/config/assets/assets_images.dart';
+import 'package:vibrona/data/models/auth/signin_user_req.dart';
+import 'package:vibrona/domain/repostiory/auth/auth_repo.dart';
 import 'package:vibrona/presentation/auth/pages/sign_up_page.dart';
+import 'package:vibrona/presentation/root/pages/root_page.dart';
+import 'package:vibrona/service_lecator.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -14,6 +18,17 @@ class SignInPage extends StatefulWidget {
 
 class _SignInPageState extends State<SignInPage> {
   bool isobscure = true;
+  String? email;
+  String? password;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,11 +48,19 @@ class _SignInPageState extends State<SignInPage> {
             ),
             const SizedBox(height: 60),
             BasicTextField(
+              controller: _emailController,
+              onChanged: (value) {
+                email = value;
+              },
               text: 'Enter user name or email',
               icon: Icon(Icons.email_outlined),
             ),
             const SizedBox(height: 24),
             BasicTextField(
+              controller: _passwordController,
+              onChanged: (value) {
+                password = value;
+              },
               isobscure: isobscure,
               text: 'Enter password',
               icon: IconButton(
@@ -54,7 +77,31 @@ class _SignInPageState extends State<SignInPage> {
               ),
             ),
             const SizedBox(height: 40),
-            BasicAppButton(text: 'Sign in', onPressed: () {}),
+            BasicAppButton(
+              text: 'Sign in',
+              onPressed: () async {
+                final result = await sl<AuthRepo>().signIn(
+                  SigninUserReq(email: email!, password: password!),
+                );
+                result.fold(
+                  (l) {
+                    ScaffoldMessenger.of(
+                      context,
+                    ).showSnackBar(SnackBar(content: Text(l)));
+                  },
+                  (r) {
+                    ScaffoldMessenger.of(
+                      context,
+                    ).showSnackBar(SnackBar(content: Text(r)));
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const RootPage()),
+                      (route) => false,
+                    );
+                  },
+                );
+              },
+            ),
             const SizedBox(height: 120),
             // Spacer(),
             Row(
--- lib/service_lecator.dart
+++ lib/service_lecator.dart
@@ -3,6 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:vibrona/data/repostiory/auth/auth_repo_impl.dart';
 import 'package:vibrona/data/sources/auth/auth_firebase_service.dart';
 import 'package:vibrona/domain/repostiory/auth/auth_repo.dart';
+import 'package:vibrona/domain/usecases/auth/sign_in_use_case.dart';
 import 'package:vibrona/domain/usecases/auth/sign_up_use_case.dart';
 
 final sl = GetIt.instance;
@@ -14,4 +15,5 @@ Future<void> intializeDependencies() async {
   sl.registerSingleton<AuthRepo>(AuthRepoImpl());
   //UseCase
   sl.registerLazySingleton<SignUpUseCase>(() => SignUpUseCase());
+  sl.registerLazySingleton<SignInUseCase>(() => SignInUseCase());
 }
