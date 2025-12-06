1f956023f814776277e1c078b3e6620f9230ddc8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-06 11:19:54 +0200

    finsh sign method in auth firebase auth service

--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,15 +1,31 @@
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:vibrona/data/models/auth/create_user_req.dart';
+import 'package:vibrona/data/models/auth/signin_user_req.dart';
 
 abstract class AuthFirebaseService {
   Future<Either> signUp(CreateUserReq createUserReq);
-  Future<void> signIn();
+  Future<Either> signIn(SigninUserReq signinUserReq);
 }
 
 class AuthFirebaseServiceImpl implements AuthFirebaseService {
   @override
-  Future<void> signIn() async {}
+  Future<Either> signIn(SigninUserReq signinUserReq) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: signinUserReq.email,
+        password: signinUserReq.password,
+      );
+      return Right('User signed in successfully');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        return Left('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        return Left('Wrong password provided for that user.');
+      }
+      return Left(e.message ?? 'An unknown error occurred');
+    }
+  }
 
   @override
   Future<Either> signUp(CreateUserReq createUserReq) async {
