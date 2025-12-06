6e7d1df03908343bfb9857df49571494b22f0e99
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-06 09:10:42 +0200

    feat: implement dependency injection for Auth services and repository

--- lib/service_option.dart
+++ lib/service_option.dart
@@ -1,1 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:vibrona/data/repostiory/auth/auth_repo_impl.dart';
+import 'package:vibrona/data/sources/auth/auth_firebase_service.dart';
+import 'package:vibrona/domain/repostiory/auth/auth_repo.dart';
 
+final sl = GetIt.instance;
+Future<void> intializeDependencies() async {
+  //Service
+  sl.registerSingleton<AuthFirebaseService>(AuthFirebaseServiceImpl());
+
+  //Repository
+  sl.registerSingleton<AuthRepo>(AuthRepoImpl());
+}
