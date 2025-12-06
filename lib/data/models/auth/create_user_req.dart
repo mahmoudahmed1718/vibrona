69fe8c39f9178b3309a1da944685a55321c29cef
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-06 08:57:31 +0200

    feat: update signUp method to accept CreateUserReq and implement user registration

---
+++ lib/data/models/auth/create_user_req.dart
@@ -0,0 +1,15 @@
+class CreateUserReq {
+  final String username;
+  final String password;
+  final String email;
+
+  CreateUserReq({
+    required this.username,
+    required this.password,
+    required this.email,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {'username': username, 'password': password, 'email': email};
+  }
+}
--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,18 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:vibrona/data/models/auth/create_user_req.dart';
+
 abstract class AuthFirebaseService {
-  Future<void> signUp();
+  Future<void> signUp(CreateUserReq createUserReq);
   Future<void> signIn();
 }
 
 class AuthFirebaseServiceImpl implements AuthFirebaseService {
   @override
-  Future<void> signIn() {
-    // TODO: implement signIn
-    throw UnimplementedError();
-  }
+  Future<void> signIn() async {}
 
   @override
-  Future<void> signUp() {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  Future<void> signUp(CreateUserReq createUserReq) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: createUserReq.email,
+        password: createUserReq.password,
+      );
+    } on FirebaseAuthException catch (e) {}
   }
 }
