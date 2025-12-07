b6208907471d209bd38a8f5d28a80ee681ddf32c
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-07 10:36:45 +0200

    feat: implement SongRepoImpl to fetch news songs from Firebase

---
+++ lib/data/repostiory/songs/song_repo_impl.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:vibrona/data/sources/songs/song_service.dart';
+import 'package:vibrona/domain/repostiory/song/song_repo.dart';
+import 'package:vibrona/service_lecator.dart';
+
+class SongRepoImpl extends SongRepo {
+  @override
+  Future<Either<dynamic, dynamic>> getNewsSongs() async {
+    return await sl<SongFirebaseService>().getNewsSongs();
+  }
+}
