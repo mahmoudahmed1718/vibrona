394b16c1122659e583b06491bc449e20914ed3a1
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-07 08:21:57 +0200

    feat: update HomePage to include body structure and integrate top card with SVG assets

--- lib/presentation/home/pages/home_page.dart
+++ lib/presentation/home/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vibrona/common/widgets/appbar/basic_appbar.dart';
 import 'package:vibrona/core/config/assets/assets_images.dart';
+import 'package:vibrona/core/config/assets/assets_vectors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,6 +12,35 @@ class HomePage extends StatelessWidget {
       appBar: BasicAppbar(
         widget: Center(child: Image.asset(AssetsImages.assetsImagesAppBarLogo)),
         isbackHide: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [_homeTopCard()],
+        ),
+      ),
+    );
+  }
+
+  Widget _homeTopCard() {
+    return Center(
+      child: SizedBox(
+        height: 150,
+        child: Stack(
+          children: [
+            Align(
+              alignment: AlignmentDirectional.bottomCenter,
+              child: SvgPicture.asset(AssetsVectors.assetsVectorsHomeTopCard),
+            ),
+            Align(
+              alignment: FractionalOffset.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 35),
+                child: Image.asset(AssetsImages.assetsImagesMahmoud),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
