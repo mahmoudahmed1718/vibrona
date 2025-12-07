31755ed005b865e67cc605f5bcb8c04e3ef85129
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-07 11:21:15 +0200

    implement method for fetch new songs in songs cubit

--- lib/presentation/home/bloc/new_song/new_songs_cubit.dart
+++ lib/presentation/home/bloc/new_song/new_songs_cubit.dart
@@ -1,10 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:vibrona/domain/entites/songs/song_entity.dart';
+import 'package:vibrona/domain/usecases/songs/get_news_song_usecase.dart';
+import 'package:vibrona/service_lecator.dart';
 
 part 'new_songs_state.dart';
 
 class NewSongsCubit extends Cubit<NewSongsState> {
   NewSongsCubit() : super(NewSongsInitial());
-  // Add
+  Future<void> fetchNewSongs() async {
+    emit(NewSongsLoading());
+    var returnIngSongs = await sl<GetNewsSongUsecase>().call();
+    returnIngSongs.fold(
+      (failure) {
+        emit(NewSongsError(message: failure.message));
+      },
+      (songs) {
+        emit(NewSongsLoaded(newSongs: songs));
+      },
+    );
+  }
 }
