5641a56494aa0f5a5dc160659375e53c4a63cdfc
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-07 08:01:32 +0200

    feat: enhance BasicAppbar to conditionally hide back button and integrate into HomePage

--- lib/common/widgets/appbar/basic_appbar.dart
+++ lib/common/widgets/appbar/basic_appbar.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:vibrona/common/helper/is_dark_mode.dart';
 
 class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
-  const BasicAppbar({super.key, this.widget});
+  const BasicAppbar({super.key, this.widget, this.isbackHide = false});
   final Widget? widget;
+  final bool isbackHide;
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -11,28 +12,30 @@ class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
       elevation: 0,
       centerTitle: true,
       title: widget,
-      leading: IconButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        icon: Container(
-          height: 50,
-          width: 50,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: context.isDarkMode
-                ? Colors.white.withValues(alpha: 0.03)
-                : Colors.black.withValues(alpha: 0.04),
-          ),
-          child: Center(
-            child: Icon(
-              Icons.arrow_back_ios,
-              color: context.isDarkMode ? Colors.white : Colors.black,
-              size: 15,
+      leading: isbackHide
+          ? null
+          : IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: context.isDarkMode
+                      ? Colors.white.withValues(alpha: 0.03)
+                      : Colors.black.withValues(alpha: 0.04),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: context.isDarkMode ? Colors.white : Colors.black,
+                    size: 15,
+                  ),
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 
--- lib/presentation/home/pages/home_page.dart
+++ lib/presentation/home/pages/home_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:vibrona/common/widgets/appbar/basic_appbar.dart';
+import 'package:vibrona/core/config/assets/assets_images.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: BasicAppbar(
+        widget: Center(child: Image.asset(AssetsImages.assetsImagesAppBarLogo)),
+        isbackHide: true,
+      ),
+    );
   }
 }
