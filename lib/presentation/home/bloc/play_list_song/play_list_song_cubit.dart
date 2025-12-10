c5307270e8de42b79f6f77f4088bd9146ad8913f
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-10 16:32:30 +0200

    implement method for fetch play list song

--- lib/presentation/home/bloc/play_list_song/play_list_song_cubit.dart
+++ lib/presentation/home/bloc/play_list_song/play_list_song_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:vibrona/domain/entites/songs/song_entity.dart';
+import 'package:vibrona/domain/usecases/songs/get_playlist_song_use_case.dart';
+import 'package:vibrona/service_lecator.dart';
 
 part 'play_list_song_state.dart';
 
 class PlayListSongCubit extends Cubit<PlayListSongState> {
   PlayListSongCubit() : super(PlayListSongInitial());
+  Future<void> fetchPlayListSongs() async {
+    emit(PlayListSongLoading());
+    var returningSongs = await sl<GetPlaylistSongUseCase>().call();
+    returningSongs.fold(
+      (failure) {
+        emit(PlayListSongError(message: failure.message));
+      },
+      (songs) {
+        emit(PlayListSongLoaded(playListSongs: songs));
+      },
+    );
+  }
 }
