dd9fa318fc249df4593b85f9ab1128d5157bd602
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-12-10 16:34:22 +0200

    implemnt provider and bloc builser for play list song file

--- lib/presentation/home/widgets/playlist_song.dart
+++ lib/presentation/home/widgets/playlist_song.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vibrona/presentation/home/bloc/play_list_song/play_list_song_cubit.dart';
 
 class PlaylistSong extends StatelessWidget {
   const PlaylistSong({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      create: (context) => PlayListSongCubit()..fetchPlayListSongs(),
+      child: BlocBuilder<PlayListSongCubit, PlayListSongState>(
+        builder: (context, state) {
+          return Container();
+        },
+      ),
+    );
   }
 }
